96c006c793db2de7920e83aa22a3fc4bede6247c
jamoroch <jamoroch@gmail.com>
2021-08-26 23:12:38 +0200

    good enough

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import { generateId } from './generate.js';
+import bodyParser from 'body-parser';
 const port = 8888;
-const foo = generateId({ length: 10, type: 'distinguishable' }, { format: 'AKA_PROD_ID' });
-console.log(foo);
+
 const app = express();
-app.post('/product/id', (req, res) => { res.send(foo) });
+app.use(bodyParser.json());
+app.post('/product/id', (req, res) => {
+    console.log(`req.body: ${JSON.stringify(req.body)}`)
+    const format = req.body.format;
+    const formatOptions = { format };
+    const length = req.body.generationSchema && req.body.generationSchema == 'FOO' ? 10 : req.body.generationSchema && req.body.generationSchema == 'BAR' ? 5 : -1;
+    const type = req.body.generationSchema && req.body.generationSchema == 'FOO' ? 'distinguishable' : req.body.generationSchema && req.body.generationSchema == 'BAR' ? 'numeric' : -1;
+    const generationOptions = {
+        batchQuantity: req.body.batchQuantity || 0,
+        length,
+        type
+    }
+    const foo = generateId(generationOptions, formatOptions);
+    foo.creationDateTime = new Date();
+    console.log(foo);
+    res.send(foo)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- src/generate.js
+++ src/generate.js
@@ -1,11 +1,25 @@
 import cryptoRandomString from 'crypto-random-string';
+const idPrefix = '_';
 export const generateId = (generationOptions, formatOptions) => {
-    const id = cryptoRandomString(generationOptions);
-    const formattedId = formatId(id, formatOptions);
-    return {
-        id,
-        formattedId,
-        creationDateTime: new Date()
+    const numberOfIds = generationOptions.batchQuantity || 0;
+    if (numberOfIds > 1) {
+        const ids = [];
+        for (let i = 0; i < numberOfIds; i++) {
+            const id = cryptoRandomString(generationOptions);
+            ids.push({
+                id: `${idPrefix}${id}`,
+                formattedId: formatId(id, formatOptions)
+            });
+        }
+        return {
+            ids
+        };
+    } else {
+        const id = cryptoRandomString(generationOptions);
+        return {
+            id: `${idPrefix}${id}`,
+            formattedId: formatId(id, formatOptions)
+        }
     }
 };
 
@@ -13,12 +27,13 @@ const formatId = (id, formatOptions) => {
     const cleanIdFormat = `_${id}`;
     const format = formatOptions.format;
     const isFormat = (type) => format && format === type;
-    if (!formatOptions) {
+    console.log(JSON.stringify(formatOptions, null, 2));
+    if (!formatOptions.format) {
         return cleanIdFormat;
     } else {
-        if (isFormat('AKA_PROD_ID')) {
+        if (isFormat('FOO')) {
             const a = id.split('').reverse();
-            return `_${a[9]}${a[8]}${a[7]}-${a[6]}${a[5]}${a[4]}-${a[3]}${a[2]}${a[1]}${a[0]}`;
+            return `${idPrefix}${a[9]}${a[8]}${a[7]}-${a[6]}${a[5]}${a[4]}-${a[3]}${a[2]}${a[1]}${a[0]}`;
         } else if (isFormat('CLEAN')) {
             return cleanIdFormat;
         } else {
